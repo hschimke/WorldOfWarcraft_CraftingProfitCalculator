908a1d5e301b77c1ad3d98c7fcf62ba02ac7ce4c
Henry Schimke <henry@azule.info>
2020-12-27 14:54:26 -0600

    await in in might not work

--- wow_crafting_profits.js
+++ wow_crafting_profits.js
@@ -241,8 +241,9 @@ async function performProfitAnalysis(region, server, character_professions, item
 
         // Get prices for BOM
         let bom_prices = [];
-        item_bom.forEach((id) => {
-            if( await checkIsCrafting( id, character_professions ) ){
+        item_bom.forEach( (id) => {
+            const is_craftable = await checkIsCrafting( id, character_professions );
+            if( is_craftable ){
                 bom_prices.push( await performProfitAnalysis( region, server, character_professions, item ) );
             }else{
                 bom_prices.push( await getAHItemPrice( id ) );
