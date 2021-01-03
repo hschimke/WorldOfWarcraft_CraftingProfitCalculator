8faaad7bffb7528f186758f0c3242154122373d4
= <=>
2021-01-03 00:58:55 +0000

    update

--- wow_crafting_profits.js
+++ wow_crafting_profits.js
@@ -15,6 +15,7 @@ const logger = winston.createLogger({
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
         new winston.transports.File({ filename: 'combined.log' }),
     ],
+
 });
 
 //
