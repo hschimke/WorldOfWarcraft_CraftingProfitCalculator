059ca8cb282cfefdfff6e7c40fa70f9ab8beab29
Henry Schimke <henry@azule.info>
2021-01-05 23:42:28 +0000

    move Inventory class definition out

--- wow_crafting_profits.js
+++ wow_crafting_profits.js
@@ -1,7 +1,7 @@
 const got = require('got');
 const fs = require('fs');
 const winston = require('winston');
-const cached_data = require('./cache/cached-data-sources.js');
+const cached_data = require('./cache/cached-data-sources');
 
 const logger = winston.createLogger({
     level: 'debug',
@@ -29,6 +29,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const secrets = require('./secrets.json');
+const { Inventory } = require("./Inventory");
 const clientID = '9d85a3dfca994efa969df07bd1e47695';
 const clientSecret = secrets.keys.client_secret;
 
@@ -909,46 +910,6 @@ function build_shopping_list(intermediate_data, rank_requested){
     });
 
     return ret_list;
-}
-
-class Inventory {
-    #internal_inventory = {};
-    #inventory_overlay = {};
-
-    /*
-     Expected input is:
-     { "inventory": [
-         {
-             "id": id,
-             "quantity": count
-         }
-     ] }
-    */
-    constructor(raw_inventory_data){
-        if( raw_inventory_data != undefined ){
-        for( let item of raw_inventory_data.inventory ){
-            this.#inventory_overlay[item.id] = item.quantity;
-        }
-    }
-    }
-    itemInInventory(item_id){
-        return this.#internal_inventory.hasOwnProperty(item_id);
-    }
-    itemCount(item_id){
-        const is_in_inventory = this.itemInInventory(item_id);
-        const has_overlay = this.#inventory_overlay.hasOwnProperty(item_id);
-        let available = 0;
-        available += is_in_inventory ? this.#internal_inventory[item_id] : 0;
-        available += has_overlay ? this.#inventory_overlay[item_id] : 0;
-        
-        return available;
-    }
-    adjustInventory(item_id, adjustment_delta){
-        if( !this.#inventory_overlay.hasOwnProperty(item_id) ){
-            this.#inventory_overlay[item_id] = 0;
-        }
-        this.#inventory_overlay[item_id] += adjustment_delta;
-    }
 }
 
 //outline
