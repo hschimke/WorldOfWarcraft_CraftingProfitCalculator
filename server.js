8c38fbf78d6797cdcfa4e372e926664b4da32fdb
Henry Schimke <henry@azule.info>
2021-01-12 18:28:25 +0000

    Fix passing off non-numerical items

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve('html/json_form.html'));
 });
 
-app.get('/custom', (req,res)=>{
+app.get('/custom', (req, res) => {
     res.sendFile(path.resolve('html/custom_run_form.html'));
 });
 
 app.post('/show_output', (req, res) => {
-    let json_data = {inventory:[]};
-    if(req.body.addon_data.length > 0){
-    json_data = JSON.parse(req.body.addon_data);
+    let json_data = { inventory: [] };
+    if (req.body.addon_data.length > 0) {
+        json_data = JSON.parse(req.body.addon_data);
     }
     let config = {};
-    if( req.body.type == 'custom'){
+    if (req.body.type == 'custom') {
         config = new RunConfiguration({
             inventory: json_data.inventory,
             professions: JSON.parse(req.body.professions),
@@ -30,9 +30,9 @@ app.post('/show_output', (req, res) => {
                 realm_name: req.body.server,
                 region_name: req.body.region,
             },
-        },Number(req.body.item_id),req.body.count);
-    }else if( req.body.type == 'json'){
-        config = new RunConfiguration(json_data,req.body.item_id, 1);
+        }, req.body.item_id, Number(req.body.count));
+    } else if (req.body.type == 'json') {
+        config = new RunConfiguration(json_data, req.body.item_id, 1);
     }
     CraftingProfitCalculator.runWithJSONConfig(config).then((data) => {
         const { price, intermediate, formatted } = data;
