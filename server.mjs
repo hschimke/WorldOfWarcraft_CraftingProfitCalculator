f22541bbf53b14e40a77157d238fcc907ce91b25
Henry Schimke <henry@azule.info>
2021-01-23 17:12:30 +0000

    json data endpoint

--- server.mjs
+++ server.mjs
@@ -56,6 +56,35 @@ app.post('/show_output', (req, res) => {
     });
 });
 
+app.post('/json_output', (req, res) => {
+    let json_data = { inventory: [] };
+    if (req.body.addon_data.length > 0) {
+        json_data = JSON.parse(req.body.addon_data);
+    }
+    let config = {};
+    if (req.body.type == 'custom') {
+        config = new RunConfiguration({
+            inventory: json_data.inventory,
+            professions: JSON.parse(req.body.professions),
+            realm: {
+                realm_name: req.body.server,
+                region_name: req.body.region,
+            },
+        }, req.body.item_id, Number(req.body.count));
+        logger.debug(`Custom search for item: ${req.body.item_id}, server: ${req.body.server}, region: ${req.body.region}, professions: ${req.body.professions}. JSON DATA: ${json_data.inventory.length}`);
+    } else if (req.body.type == 'json') {
+        logger.debug('json search');
+        config = new RunConfiguration(json_data, req.body.item_id, Number(req.body.needed));
+        config.region
+        logger.debug(`JSON search for item: ${config.item_id}, server: ${config.realm_name}, region: ${config.realm_region}, professions: ${config.professions}. JSON DATA: ${json_data.inventory.length}`);
+    }
+
+    runWithJSONConfig(config).then((data) => {
+        const { price, intermediate, formatted } = data;
+        res.send(intermediate);
+    });
+});
+
 const server = app.listen(port, () => {
     logger.info(`Crafting Profit Calculator running at: http://localhost:${port}`)
 });
