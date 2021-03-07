c037d9126fb29e6dbec28e4ffdf69ad3487f8d4b
Henry Schimke <henry@azule.info>
2021-03-07 04:41:31 +0000

    Add scatter from google-react-charts

--- client/src/Auctions.js
+++ client/src/Auctions.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Auctions.css';
 import { apiAuctionHistoryFetch } from './ApiClient.js';
+import { Chart } from "react-google-charts";
 
 class Auctions extends React.Component {
     constructor(props) {
@@ -15,6 +16,7 @@ class Auctions extends React.Component {
             item_name: 'Grim-Veiled Bracers',
             realm_name: 'Hyjal',
             region: 'US',
+            chart_ready: false,
         };
     }
 
@@ -45,11 +47,12 @@ class Auctions extends React.Component {
         this.setState({ raw_data: data });
 
         const chart_data = [['Downloaded', 'Price']].concat(data.map(element => {
-            return [element.downloaded, element.price];
+            return [new Date(element.downloaded), element.price];
         }));
+        this.setState({ chart_ready: true });
         this.setState({ chart_data: chart_data });
     }
-
+    // https://react-google-charts.com/scatter-chart
     render() {
         return (
             <div className="Auctions">
@@ -69,6 +72,23 @@ class Auctions extends React.Component {
                     <button type="submit" disabled={!this.state.button_enabled} value="Run">Run</button>
                 </form>
                 <div className="RawReturn">
+                    {
+                        (this.state.chart_ready === true) &&
+                        <Chart
+                            width={'600px'}
+                            height={'400px'}
+                            chartType="ScatterChart"
+                            loader={<div>Loading Chart</div>}
+                            data={this.state.chart_data}
+                            options={{
+                                title: 'Price Over Time',
+                                hAxis: { title: 'Time' },
+                                vAxis: { title: 'Price' },
+                                legend: 'none',
+                            }}
+                            rootProps={{ 'data-testid': '1' }}
+                        />
+                    }
                     <pre>
                         {JSON.stringify(this.state.chart_data, undefined, 2)}
                     </pre>
