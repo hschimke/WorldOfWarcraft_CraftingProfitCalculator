d74c6a1eb87cba156989a2c37fee3a0f88deaaa4
Henry Schimke <henry@azule.info>
2021-03-15 15:21:41 +0000

    Move dispatch logic and stop passing onchange

--- client/src/CraftingProfits/RunForm.js
+++ client/src/CraftingProfits/RunForm.js
@@ -4,6 +4,40 @@ import { useContext } from 'react';
 import { RegionSelector } from '../Shared/RegionSelector.js';
 
 function RunForm(props) {
+    switch (props.form_type) {
+        case 'advanced':
+            return <AdvancedRunFrom {...props} />
+        case 'simple':
+            return <SimpleRunFrom {...props} />
+        default:
+            throw new Error();
+    }
+}
+
+function SimpleRunFrom(props) {
+    const dispatch = useContext(CraftingProfitsDispatch);
+
+    const handleInputChange = (event) => {
+        dispatch({ field: event.target.name, value: event.target.value });
+    };
+    return (
+        <form onSubmit={props.handleSubmit} className="RunForm">
+            <label>Item:
+                    <input type="text" name="item" value={props.item} onChange={handleInputChange} />
+            </label>
+            <label>Required Count:
+                    <input type="text" name="required" value={props.required} onChange={handleInputChange} />
+            </label>
+            <label>Addon Data
+                    <textarea name="addon_data" rows="5" cols="100" value={props.addon_data} onChange={handleInputChange} />
+            </label>
+            <button type="submit" disabled={!props.button_enabled} value="Run">Run</button>
+        </form >
+    );
+}
+
+function AdvancedRunFrom(props) {
+    const profession_list = props.allProfessions;
     const dispatch = useContext(CraftingProfitsDispatch);
 
     const handleInputChange = (event) => {
@@ -13,47 +47,17 @@ function RunForm(props) {
     const handleCheckbox = (event) => {
         dispatch({ field: 'professions', value: event.target.name });
     };
-
-    switch (props.form_type) {
-        case 'advanced':
-            return <AdvancedRunFrom {...props} handleInputChange={handleInputChange} handleCheckbox={handleCheckbox} />
-        case 'simple':
-            return <SimpleRunFrom {...props} handleInputChange={handleInputChange} />
-        default:
-            throw new Error();
-    }
-}
-
-function SimpleRunFrom(props) {
     return (
         <form onSubmit={props.handleSubmit} className="RunForm">
             <label>Item:
-                    <input type="text" name="item" value={props.item} onChange={props.handleInputChange} />
+                    <input type="text" name="item" value={props.item} onChange={handleInputChange} />
             </label>
-            <label>Required Count:
-                    <input type="text" name="required" value={props.required} onChange={props.handleInputChange} />
-            </label>
-            <label>Addon Data
-                    <textarea name="addon_data" rows="5" cols="100" value={props.addon_data} onChange={props.handleInputChange} />
-            </label>
-            <button type="submit" disabled={!props.button_enabled} value="Run">Run</button>
-        </form >
-    );
-}
-
-function AdvancedRunFrom(props) {
-    const profession_list = props.allProfessions;
-    return (
-        <form onSubmit={props.handleSubmit} className="RunForm">
-            <label>Item:
-                    <input type="text" name="item" value={props.item} onChange={props.handleInputChange} />
-            </label>
-            <RegionSelector selected_region={props.region} onChange={props.handleInputChange} />
+            <RegionSelector selected_region={props.region} onChange={handleInputChange} />
             <label>Server:
-                    <input type="text" name="realm" value={props.realm} onChange={props.handleInputChange} />
+                    <input type="text" name="realm" value={props.realm} onChange={handleInputChange} />
             </label>
             <label>Required Count:
-                    <input type="text" name="required" value={props.required} onChange={props.handleInputChange} />
+                    <input type="text" name="required" value={props.required} onChange={handleInputChange} />
             </label>
             <fieldset className="Professions">
                 <span>Professions:</span>
@@ -61,13 +65,13 @@ function AdvancedRunFrom(props) {
                     return (
                         <label key={`${item}key`}>
                             {item}:
-                            <input type="checkbox" name={item} checked={!!props.professions.includes(item)} onChange={props.handleCheckbox} />
+                            <input type="checkbox" name={item} checked={!!props.professions.includes(item)} onChange={handleCheckbox} />
                         </label>
                     );
                 })}
             </fieldset>
             <label>Addon Data
-                    <textarea name="addon_data" rows="5" cols="100" value={props.addon_data} onChange={props.handleInputChange} />
+                    <textarea name="addon_data" rows="5" cols="100" value={props.addon_data} onChange={handleInputChange} />
             </label>
             <button type="submit" disabled={!props.button_enabled} value="Run">Run</button>
         </form >
