b8963a8ba06cb2253d60c5a27cc5e3ef1e4ce60d
Henry Schimke <henry@azule.info>
2021-03-26 03:00:10 +0000

    staged from pg

--- hourly-injest.js
+++ hourly-injest.js
@@ -1,7 +1,43 @@
-import { scanRealms, archiveAuctions } from './auction-history.js';
+import { scanRealms, archiveAuctions, addRealmToScanList, fillNItems } from './auction-history.js';
+import { parentLogger } from './logging.js';
 
-await scanRealms();
+const logger = parentLogger.child();
 
-//if ((new Date()).getHours() === 0) {
-    await archiveAuctions();
-//}
+const server_mode = process.env.STANDALONE_CONTAINER === undefined ? 'normal' : process.env.STANDALONE_CONTAINER;
+
+let standalone_container_abc = undefined;
+
+//await addRealmToScanList('hyjal','us');
+
+async function job() {
+    logger.info('Starting hourly injest job.');
+    const run_list = [];
+
+    run_list.push(scanRealms());
+
+    if ((new Date()).getHours() === 0) {
+        logger.info('Performing daily archive.');
+        run_list.push(archiveAuctions());
+    }
+
+    run_list.push(fillNItems());
+
+    await Promise.all(run_list);
+    logger.info('Finished hourly injest job.');
+}
+
+switch (server_mode) {
+    case 'hourly':
+        logger.info('Started in default mode. Running job and exiting.');
+        await job();
+        break;
+    case 'standalone':
+        logger.info('Started in standalone container mode. Scheduling hourly job.');
+        standalone_container_abc = setInterval(job, 3.6e+6);
+        standalone_container_abc.unref();
+        break;
+    case 'normal':
+    default:
+        logger.info('Started in normal mode taking no action.');
+        break;
+}
