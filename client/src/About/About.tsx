374c996d291ed30c88ad62483ba3ff3e8b590a38
Henry Schimke <henry@azule.info>
2021-12-28 02:27:30 +0000

    More About and Footer

--- client/src/About/About.tsx
+++ client/src/About/About.tsx
@@ -4,6 +4,8 @@ import './About.css';
 function About() {
     const download_uri = '/addon-download';
     const full_about_uri = 'https://github.com/hschimke/WorldOfWarcraft_CraftingProfitCalculator/blob/main/README.md';
+    const source_uri = 'https://github.com/hschimke/WorldOfWarcraft_CraftingProfitCalculator';
+    const bugs_uri = 'https://github.com/hschimke/WorldOfWarcraft_CraftingProfitCalculator/issues';
     return (
         <div id="About">
             <span className='header'>About</span>
@@ -16,7 +18,15 @@ function About() {
                 <li>China is no fully supported.</li>
                 <li>Internationalization is not supported and all text is in en_us.</li>
                 <li>All searches are done in en_us.</li>
+                <li>Some items are incorrectly listed as being available from vendors,
+                    this can confuse the system. If you find one of these please <a href={bugs_uri}>report it</a>.</li>
             </ul>
+
+            <span className='header'>Privacy</span>
+            <p>wowcpc.info does not collect any personal information and requires no login.
+                Basic server logs of errors may be kept, but ip addresses or other user identifiable information is not.
+                You can check our work by looking at <a href={source_uri}>the source</a>, which is exactly what we deply.
+            </p>
 
             <span className='header'>Links</span>
             <a href={full_about_uri}>Full Readme [github]</a>
--- client/src/Footer/Footer.tsx
+++ client/src/Footer/Footer.tsx
@@ -4,13 +4,13 @@ function Footer() {
     const source_uri = 'https://github.com/hschimke/WorldOfWarcraft_CraftingProfitCalculator';
     const bugs_uri = 'https://github.com/hschimke/WorldOfWarcraft_CraftingProfitCalculator/issues';
     return (
-        <div className="Footer">
+        <footer>
             <ul>
                 <li>Copyright 2021</li>
                 <li><a href={source_uri}>Source</a></li>
                 <li><a href={bugs_uri}>Report Bugs</a></li>
             </ul>
-        </div>
+        </footer>
     )
 }
 
