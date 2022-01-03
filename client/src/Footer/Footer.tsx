185776998958cd9b7cc91b71a70ae4214fdbce37
Henry Schimke <henry@azule.info>
2022-01-03 16:55:49 +0000

    Footer disclaimer

--- client/src/Footer/Footer.tsx
+++ client/src/Footer/Footer.tsx
@@ -1,14 +1,15 @@
-import './Footer.css';
+import styles from './Footer.module.css';
 import {BugReportLink,CopyRightTag,SourceLink} from '../Shared/Links';
 
 function Footer() {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <ul>
                 <li><CopyRightTag /></li>
                 <li><SourceLink text="Source" /></li>
                 <li><BugReportLink text="Report Bugs" /></li>
             </ul>
+            <p>"World of Warcraft" is a trademark of Activision Blizzard. This is only a fansite, there is no affiliation between wowcpc.info and Activision Blizzard.</p>
         </footer>
     )
 }
