9d5dc166643546d1c81b03792e49ed2190145da6
Henry Schimke <henry@azule.info>
2022-01-07 17:52:07 +0000

    spelling corrections in about

--- client/src/About/About.tsx
+++ client/src/About/About.tsx
@@ -1,11 +1,11 @@
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import styles from './About.module.css';
 import { BugReportLink, SourceLink, ReadMeLink, AddonDownloadLink } from '../Shared/Links';
 
 function About() {
-    useEffect(()=>{
+    useEffect(() => {
         document.title = "Crafting Profits Calculator - About";
-    },[]);
+    }, []);
 
     return (
         <div className={styles.About}>
@@ -15,8 +15,8 @@ function About() {
 
             <span className={styles.header}>Known Issues</span>
             <ul>
-                <li>The behaviour of some international servers is undefined.</li>
-                <li>China is no fully supported.</li>
+                <li>The behavior of some international servers is undefined.</li>
+                <li>China is not fully supported.</li>
                 <li>Internationalization is not supported and all text is in en_us.</li>
                 <li>All searches are done in en_us.</li>
                 <li>Some items are incorrectly listed as being available from vendors,
@@ -26,12 +26,13 @@ function About() {
             <span className={styles.header}>Privacy</span>
             <p>wowcpc.info does not collect any personal information and requires no login.
                 Basic server logs of errors may be kept, but ip addresses or other user identifiable information is not.
-                You can check our work by looking at <SourceLink text="the source" />, which is exactly what we deply.
+                You can check our work by looking at <SourceLink text="the source" />, which is exactly what we deploy.
             </p>
-            <p>wowcpc.info uses Google Analytics to record pageviews and metrics.
+            <p>wowcpc.info uses Google Analytics to record page-views and metrics.
                 We have disabled all data sharing and advertising features.
                 In addition we have enabled anonymized ip-addresses.
             </p>
+
 
             <span className={styles.header}>Links</span>
             <ul>
